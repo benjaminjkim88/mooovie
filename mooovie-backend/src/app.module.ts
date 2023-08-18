af7169fdb14074a09485b321ca2f0f65e2563a02
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-08-18 16:31:24 -0400

    created reviews module

--- mooovie-backend/src/app.module.ts
+++ mooovie-backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TopMoviesModule } from './top-movies/top-movies.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [TopMoviesModule],
+  imports: [TopMoviesModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
