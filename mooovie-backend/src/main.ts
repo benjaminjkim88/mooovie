fc6079cffa38aa7222a00a8747a4ada3c0c6635c
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-08-23 16:57:45 -0400

    fixed cors bug for backend

--- mooovie-backend/src/main.ts
+++ mooovie-backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
